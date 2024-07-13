933286b21e2e7197dc5b92bf42a5f9354abd7641
Chau Hoang Hieu Lam <lamchauhoanghieu@gmail.com>
2024-07-13 18:19:36 +1000

    fix: URL error

--- src/app/post-campaign/post-campaign.component.ts
+++ src/app/post-campaign/post-campaign.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-post-campaign',
   templateUrl: './post-campaign.component.html',
   styleUrls: ['./post-campaign.component.css'],
-  imports: [FormsModule]
+  imports: [CommonModule, FormsModule],
 })
 export class PostCampaignComponent {
   title = '';
@@ -20,12 +21,14 @@ export class PostCampaignComponent {
   constructor(private http: HttpClient, private router: Router) {}
 
   postCampaign() {
-    this.http.post('/.netlify/functions/api/campaign', {
-      title: this.title,
-      description: this.description,
-      author: this.author
-    }).subscribe((response: any) => {
-      this.router.navigate(['/success']);
-    });
+    this.http
+      .post('https://social-awareness-backendapp.netlify.app/api/campaign', {
+        title: this.title,
+        description: this.description,
+        author: this.author,
+      })
+      .subscribe((response: any) => {
+        this.router.navigate(['/success']);
+      });
   }
 }
